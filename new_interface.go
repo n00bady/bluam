508d8e4f47c0b2b7ddd1e13bc042c829fa2858a6
n00bady <kaz00@vivaldi.net>
2024-07-03 20:30:54 +0300

    Started the changes requested, the blocklists now are specified in blocking.json and this is unfolded into a struct wich is used to download them and then merge them into unified lists per category according to their tags.Also some other changes so the rest of the code can work with new_interface.go

--- new_interface.go
+++ new_interface.go
@@ -1,11 +1,71 @@
 package main
 
-type DNSConfig struct{}
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
 
-func LoadConfig(path string) {
+type DNSConfig struct {
+	LogBlocked bool         `json:"LogBlocked"`
+	LogAll     bool         `json:"LogAll"`
+	IP         string       `json:"IP"`
+	Port       int          `json:"Port"`
+	DNSServers []string     `json:"DNSServers"`
+	ListPath   string       `json:"ListPath"`
+	Lists      []blockLists `json:"Lists"`
+}
+
+type blockLists struct {
+	Tag     string `json:"Tag"`
+	Url     string `json:"URL"`
+	Enabled bool   `json:"Enabled"`
+}
+
+func LoadConfig(path string) *DNSConfig {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal("Error reading config file:", err)
+	}
+
+	var config DNSConfig
+	err = json.Unmarshal(bytes, &config)
+	if err != nil {
+		log.Fatal("Error unmarshaling json:", err)
+	}
+
+	return &config
 }
 
 func UpdateListsAndMergeTags(config *DNSConfig) {
+	// imo it's better to overwrite the old blocklists
+	// rather than delete and then re-download
+	// because if one of the list sources is temporary
+	// or permantly unavailable we can still use the last version
+	for _, l := range config.Lists {
+		DownloadBlocklist(l.Tag, l.Url)
+	}
+
+	// reads all the the files in each category in downloaded blocklists
+	// directory and merge them
+	dl_path := filepath.Join(config.ListPath, "dl_blocklists")
+	subDirs, _ := os.ReadDir(dl_path)
+	fmt.Println("Merging blocklists...")
+	for _, d := range subDirs {
+		cat := d.Name()
+		var locations []string
+		if d.IsDir() {
+			subItems, _ := os.ReadDir(filepath.Join(dl_path, d.Name()))
+			for _, fN := range subItems {
+				locations = append(locations, filepath.Join(dl_path, d.Name(), fN.Name()))
+			}
+		}
+		MergeBlocklists(cat, locations)
+		fmt.Println("\t", cat, "blocklists merged!")
+		locations = nil
+	}
 }
 
 func DisableList(config *DNSConfig) {
